07d26a0d6f92ddcde1ae9565fa53e189e96c0f77
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2023-06-25 16:52:45 +0200

    Add ExtendedMath utils class with method to calculate GCD

--- media/src/main/java/com/akmal2409/github/netflix/media/commons/ExtendedMath.java
+++ media/src/main/java/com/akmal2409/github/netflix/media/commons/ExtendedMath.java
@@ -1,2 +1,37 @@
-package com.akmal2409.github.netflix.media.commons;public class ExtendedMath {
+package com.akmal2409.github.netflix.media.commons;
+
+/**
+ * Utility class providing extended mathematical computations.
+ */
+public final class ExtendedMath {
+
+  private ExtendedMath() { throw new IllegalStateException("Cannot construct an instance of the utility class"); }
+
+  /**
+   * Computes Greatest Common Divisor using Euclidean algorithm.
+   * The formula is following gcd(a, b) = gcd(a, a mod b) where a >= b and mod is modulo (remainder).
+   * We can do it iteratively till we get a remainder of 0, then the previous remainder is our GCD.
+   *
+   * @param a first number
+   * @param b second number
+   * @return gcd - greatest common divisor.
+   */
+  public static int gcd(int a, int b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+
+    if (b > a) return gcd(b, a);
+
+    int lastRemainder = b;
+    int currentRemainder = a % b;
+
+    while (currentRemainder > 0) {
+      lastRemainder = currentRemainder;
+
+      currentRemainder = a % b;
+      b = currentRemainder;
+    }
+
+    return lastRemainder;
+  }
 }
--- media/src/test/java/com/akmal2409/github/netflix/media/commons/ExtendedMathTest.java
+++ media/src/test/java/com/akmal2409/github/netflix/media/commons/ExtendedMathTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.akmal2409.github.netflix.media.commons;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class ExtendedMathTest {
-  
+
+  @Test
+  @DisplayName("gcd function computes correct result when second number is greater than the first")
+  void gcdCorrectResultForSecondNumberLarger() {
+    assertGCD(12, 16, 4);
+  }
+  @Test
+  @DisplayName("gcd function computes correct results for two positive integers")
+  void gcdCorrectResultForTwoPositiveIntegers() {
+    assertGCD(16, 12, 4);
+  }
+
+  @Test
+  @DisplayName("gcd function computes correct results for two negative integers")
+  void gcdCorrectResultForTwoNegativeIntegers() {
+    assertGCD(-16, -12, 4);
+  }
+
+  @Test
+  @DisplayName("gcd function computes correct results for negative and positive integers")
+  void gcdCorrectResultForNegativeAndPositiveInteger() {
+    assertGCD(-16, 12, 4);
+  }
+
+  private static void assertGCD(int firstNumber, int secondNumber, int expectedGcd) {
+    assertThat(ExtendedMath.gcd(firstNumber, secondNumber))
+        .isEqualTo(expectedGcd);
+  }
 }
