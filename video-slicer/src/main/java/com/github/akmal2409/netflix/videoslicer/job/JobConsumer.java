5f086d0f2cd08c24846198976d2291f16515c673
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2023-08-22 16:12:31 +0200

    Add cleanup stage to remove files after pipeline has finished

--- video-slicer/src/main/java/com/github/akmal2409/netflix/videoslicer/job/JobConsumer.java
+++ video-slicer/src/main/java/com/github/akmal2409/netflix/videoslicer/job/JobConsumer.java
@@ -13,11 +13,15 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import org.apache.commons.lang3.StringUtils;
@@ -83,49 +87,91 @@ public class JobConsumer extends DefaultConsumer {
 
       final var audioFilePath = outputFileDirectory.resolve("audio.m4a");
 
-      final var audioExtractionFuture = CompletableFuture.runAsync(() ->
-                                                                       videoSlicer.extractAudio(
-                                                                           videoSource.filePath(),
-                                                                           audioFilePath),
-          executor);
-
-      // while the audio task is running async, we can wait for segmentation to finish and then index all files
-      final var segmentsPath = outputFileDirectory.resolve("segments");
-      videoSlicer.slice(
-          videoSource.filePath(),
-          segmentsPath,
-          Duration.ofSeconds(manifest.segmentDurationSeconds()),
-          SegmentConstants.SEGMENT_FILE_NAME_PATTERN);
-
-      audioExtractionFuture.join();
-
-      // Now we need to get start and end frame numbers of each segment
-      final Segment[] segments = videoAnalyser.analyseSegments(segmentsPath,
-          (path) -> SegmentConstants.SEGMENT_FILE_NAME_REGEX_PATTERN.matcher(
-              path.getFileName().toString()).matches(),
-          (p1, p2) -> SegmentConstants.SEGMENT_FILE_NAME_COMPARATOR.compare(
-              p1.getFileName().toString(), p2.getFileName().toString())
-      );
-
-      final ProcessedIndex finalIndex = createCompleteFileIndex(videoIndex, segments,
-          manifest, audioFilePath.getFileName().toString()); // index will be written as json file along the files in an output bucket under index.json
-
-      final var indexFilePath = segmentsPath.getParent().resolve("index.json");
-
       try {
-        writeIndexFile(indexFilePath, finalIndex);
-      } catch (IOException e) {
-        log.error("message=Failed to write index.json on disk;job_id={};consumer_tag={}",
-            manifest.jobId(), consumerTag);
+        final var audioExtractionFuture = CompletableFuture.runAsync(() ->
+                                                                         videoSlicer.extractAudio(
+                                                                             videoSource.filePath(),
+                                                                             audioFilePath),
+            executor);
 
-        throw new JobExecutionFailureException("Failed to write index file", manifest.jobId());
+        // while the audio task is running async, we can wait for segmentation to finish and then index all files
+        final var segmentsPath = outputFileDirectory.resolve("segments");
+        videoSlicer.slice(
+            videoSource.filePath(),
+            segmentsPath,
+            Duration.ofSeconds(manifest.segmentDurationSeconds()),
+            SegmentConstants.SEGMENT_FILE_NAME_PATTERN);
+
+        audioExtractionFuture.join();
+
+        // Now we need to get start and end frame numbers of each segment
+        final Segment[] segments = videoAnalyser.analyseSegments(segmentsPath,
+            (path) -> SegmentConstants.SEGMENT_FILE_NAME_REGEX_PATTERN.matcher(
+                path.getFileName().toString()).matches(),
+            (p1, p2) -> SegmentConstants.SEGMENT_FILE_NAME_COMPARATOR.compare(
+                p1.getFileName().toString(), p2.getFileName().toString())
+        );
+
+        final ProcessedIndex finalIndex = createCompleteFileIndex(videoIndex, segments,
+            manifest, audioFilePath.getFileName()
+                          .toString()); // index will be written as json file along the files in an output bucket under index.json
+
+        final var indexFilePath = segmentsPath.getParent().resolve("index.json");
+
+        try {
+          writeIndexFile(indexFilePath, finalIndex);
+        } catch (IOException e) {
+          log.error("message=Failed to write index.json on disk;job_id={};consumer_tag={}",
+              manifest.jobId(), consumerTag);
+
+          throw new JobExecutionFailureException("Failed to write index file", manifest.jobId());
+        }
+
+        s3Store.uploadProcessedFiles(manifest.outputBucket(),
+            manifest.outputFileKeyPrefix(), outputFileDirectory);
+
+        getChannel().basicAck(envelope.getDeliveryTag(),
+            false); // acknowledge it and complete execution
+        
+      } finally {
+        cleanupFiles(videoSource.filePath().getParent());
       }
 
-      s3Store.uploadProcessedFiles(manifest.outputBucket(),
-          manifest.outputFileKeyPrefix(), outputFileDirectory);
 
-      getChannel().basicAck(envelope.getDeliveryTag(), false); // acknowledge it and complete execution
     }
+  }
+
+  /**
+   * Removes all intermediary files that were required by the pipeline.
+   * @param directory path where all the files and directories reside.
+   */
+  private void cleanupFiles(Path directory) throws IOException {
+    Files.walkFileTree(directory, Set.of(), 10, new FileVisitor<>() {
+      @Override
+      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+          throws IOException {
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        // since we delete only files on the way, we need to ensure that after we are done
+        // visiting, we also delete the folder
+        Files.delete(dir); // folder must be empty
+        return FileVisitResult.CONTINUE;
+      }
+    });
   }
 
   private void writeIndexFile(Path indexFilePath, ProcessedIndex finalIndex)
@@ -141,14 +187,14 @@ public class JobConsumer extends DefaultConsumer {
   private ProcessedIndex createCompleteFileIndex(VideoIndex videoIndex, Segment[] segments,
       PreprocessingManifest manifest, String audioFileName) {
     final ProcessedSegment[] mappedSegments = Arrays.stream(segments)
-                                                                       .map(
-                                                                           segment -> new ProcessedSegment(
-                                                                               segment.fileName(),
-                                                                               segment.index(),
-                                                                               segment.startFrame(),
-                                                                               segment.endFrame()))
-                                                                       .toArray(
-                                                                           ProcessedSegment[]::new);
+                                                  .map(
+                                                      segment -> new ProcessedSegment(
+                                                          segment.fileName(),
+                                                          segment.index(),
+                                                          segment.startFrame(),
+                                                          segment.endFrame()))
+                                                  .toArray(
+                                                      ProcessedSegment[]::new);
 
     // The distance between I-Frames (IDR), recommended to be equal for ABR streaming
     final int gopSize = (int) Math.ceil(manifest.segmentDurationSeconds() * videoIndex.frameRate());
