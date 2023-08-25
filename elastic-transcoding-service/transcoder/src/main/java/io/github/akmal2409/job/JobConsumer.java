d249250845d5abdc41881d3d57c9e487d116497f
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2023-08-25 11:37:30 +0200

    Add comments to JobConsumer code

--- elastic-transcoding-service/transcoder/src/main/java/io/github/akmal2409/job/JobConsumer.java
+++ elastic-transcoding-service/transcoder/src/main/java/io/github/akmal2409/job/JobConsumer.java
@@ -54,6 +54,8 @@ public class JobConsumer extends DefaultConsumer {
     final String[] segmentNames = manifest.segmentFileNames().clone();
     Arrays.sort(segmentNames);
 
+    // given source key points to the whole "folder" containing index file, audio and segments, we are only interested in segments
+    // that is why the path is concatenated with /segments
     final Path segmentsPath = s3Store.downloadSamePrefixFiles(
         manifest.sourceBucket(), manifest.sourceKeyPrefix().concat("/segments"),
         manifest.segmentFileNames(), manifest.jobId().toString()
@@ -63,6 +65,7 @@ public class JobConsumer extends DefaultConsumer {
         segmentNames.length, segmentsPath, manifest.jobId(), consumerTag);
 
     try {
+      // so that video is not passed through encoding without any reason (loss of quality)
       if (manifest.skipFirstFrames() > 0) {
         final var firstSegment = segmentsPath.resolve(segmentNames[0]);
         skipVideoFrames(firstSegment, manifest.skipFirstFrames());
@@ -71,12 +74,18 @@ public class JobConsumer extends DefaultConsumer {
             manifest.skipFirstFrames(), segmentNames[0], manifest.jobId(), consumerTag);
       }
 
+      // each segment is under the segments folder (the one we downloaded to) so we just need to concat those together
       final Path[] segmentPaths = buildSegmentPaths(segmentsPath, segmentNames);
+      // because we are transcoding using overlapping segments to get target duration, we need to join them into 1 file with a fixed GOP
       final Path joinedEncodedSegments = segmentsPath.resolve("joined.mp4");
 
+      // so it joins the k-segments into 1 and then cuts after GOP size and gets the first chunk, the rest is discarded as it was only used to get the target segment
       transcoder.joinVideosWithGopSize(segmentPaths, joinedEncodedSegments, manifest.gopSize());
 
       final Path processedFilesPath = segmentsPath.resolve("processed");
+
+      // the segment we have currently encoded was encoded using highest settings to preserve the quality, that is why we keep it in a separate folder
+      // with original bit rate and resolution
       final Path originalQualitySegmentPath = processedFilesPath.resolve(
           String.format("%dx%d-%d", manifest.originalQuality().width(),
               manifest.originalQuality().height(),
@@ -85,7 +94,7 @@ public class JobConsumer extends DefaultConsumer {
       final Path processedSegment = originalQualitySegmentPath
                                         .resolve(SegmentConstants.segmentIndexToFileName(
                                             manifest.segmentIndex()));
-
+      
       transcoder.extractFirstSegment(joinedEncodedSegments, processedSegment,
           manifest.targetSegmentDurationSeconds());
       log.debug(
@@ -111,6 +120,15 @@ public class JobConsumer extends DefaultConsumer {
     }
   }
 
+  /**
+   * According to void qualities specified in the manifest, it constructs the transcoding jobs to different bit rates and qualities
+   * for the {@link Transcoder} class. The output files are stored relative to the <code>contextPath</code> parameter following the naming convention:
+   * video_quality_i => $context_path/$widthx$height-$bitRate/segment-i.container
+   *
+   * @param manifest transcoding job manifest
+   * @param segmentPath path to the raw segment that need to be transcoded into multiple qualities and bit rates
+   * @param contextPath output directory where the segments are going to be placed.
+   */
   private void transcodeToDifferentBitRates(TranscodingJobManifest manifest, Path segmentPath,
       Path contextPath) {
     final VideoQualityTranscodingTask[] tasks = new VideoQualityTranscodingTask[manifest.outputQualities().length];
